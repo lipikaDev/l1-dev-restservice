95a4ae72b4e39f427d9bf604fcdb919dd440c433
LipikaB <lipika.banerjee21@gmail.com>
2019-07-04 11:09:07 -0400

    Convert Kelvin Temp to Censius added modifications to jquery static file.

--- src/main/resources/static/connection.js
+++ src/main/resources/static/connection.js
@@ -10,60 +10,32 @@ $(document).ready(function() {
 
     });
 });
-    // var httpReq = new XMLHttpRequest();
-    // var url = 'http://localhost:8080/user-info?userName=LipiB&userText=Hellothere!';
-    // // var url = 'http://localhost:8080/user-info?';
-    // // var fields = 'userName=LipiB&userText=Hello!&format=JSON';
-
-    // httpReq.open('POST', url, true);
-    // console.log('good');
-
-    // httpReq.setRequestHeader('Access-Control-Allow-Headers', '*');
-    // httpReq.setRequestHeader('Content-type', 'application/ecmascript');
-    // httpReq.setRequestHeader('Access-Control-Allow-Origin', '*');
-    
-    // // httpReq.onreadystatechange = function () {
-    // //     console.log('test');
-    // //     if (httpReq.readyState === 4 && httpReq.status === 'success') {
-    // //         console.log('test');
-    // //         alert(httpReq.responseText);
-    // //     }
-    // // };
-
-    // // httpReq.send(fields);
-
-        // var getName = document.getElementById("inputName").value;
-        // var getText = document.getElementById("inputText").value;
-        // var getCity = document.getElementById("inputCity").value;
 
 function fire_ajax_submit() {
 
-        var getName = {}
-        getName["requestUserName"] = $("#inputName").val();
-        var getText = {}
-        getText["requestUserText"] = $("#inputText").val();
-        var getCity = {}
-        getCity["inputCity"] = $("#inputCity").val();
+    var getName = {}
+    getName["requestUserName"] = $("#inputName").val();
+    var getText = {}
+    getText["requestUserText"] = $("#inputText").val();
+    var getCity = {}
+    getCity["requestUserCity"] = $("#inputCity").val();
 
-        $("#input").prop("disabled", true);
+    $("#input").prop("disabled", true);
 
     $.ajax({
-        // url: "http://rest-service.guides.spring.io/greeting"
-        type: "GET",    
+        type: "GET",
         ccontentType: "application/json",
         // url: "http://localhost:8080/user-info?requestUserName=LipiB&requestUserText=Hellothere!",
-        url: "http://localhost:8080/user-info?",
-        data: JSON.stringify(getName,getText),
+        url: "http://localhost:8080/user-info?requestUserName=" + getName["requestUserName"] + "&requestUserText=" +  getText["requestUserText"] + "&requestUserCity=" + getCity["&requestUserCity"],
         crossDomain: true,
-        dataType: 'json',
-
     }).then(function(data) {
-       $('#userdata').append($("<tr>")
-       .append($("<td>").append(data.userName))
-       .append($("<td>").append(data.userText))
-       .append($("<td>").append(data.userName))
-       .append($("<td>").append(data.userText))
-       .append($("<td>").append(data.userName))
-       .append($("<td>").append(data.userText)));
+        $('#userdata').append($("<tr>")
+            .append($("<td>").append(data.userName))
+            .append($("<td>").append(data.userText))
+            .append($("<td>").append(data.addedAtTime))
+            .append($("<td>").append(data.userCity))
+            .append($("<td>").append(data.geoLatitude))
+            .append($("<td>").append(data.geoLongitude))
+            .append($("<td>").append(data.temperatureCelsius)));
     });
 } 
